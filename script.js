b72290226c501d0a510686c9df6e68d5713c8753
Cserne <angeli.cserne@gmail.com>
2021-11-30 11:29:37 +0100

    Changes added.

--- script.js
+++ script.js
@@ -25,28 +25,51 @@ const functionName = () => {
 meghívása: functionName();
 */
 
-const input = (type, name, label) => {
+const inputElement = (type, name, label) => {
    return `
         <div>
             <label>${label}</label>
             <input type="${type}" name="${name}">
         </div>
     `
-}
+};
 
-const form = `
+const formElement = `
     <form id="form">
-        ${ input("text", "firstName", "Keresztneved") }
-        ${ input("file", "profilePicture", "Profilképed") }
-        ${ input("email", "personalEmail", "Email címed") }
-        ${ input("radio", "newsLetter", "Szeretnél-e hírlevelet kapni?") }
-        ${ input("checkbox", "terms", "Elfogadod-e a felhasználási feltételeket?") }
+        ${ inputElement("text", "firstName", "Keresztneved") }
+        ${ inputElement("file", "profilePicture", "Profilképed") }
+        ${ inputElement("email", "personalEmail", "Email címed") }
+        ${ inputElement("radio", "newsLetter", "Szeretnél-e hírlevelet kapni?") }
+        ${ inputElement("checkbox", "terms", "Elfogadod-e a felhasználási feltételeket?") }
+        <button>Ok</button>
     </form>
 `;
 
+const formSubmit = (event) => {
+    event.preventDefault(); //ez éri el, hogy az alapértelmezett submitje ne fusson le az eseménynek
+    console.log(event);
+    event.target.classList.add("submit"); //a target azért kell, hogy már ne kelljen getElementBYId-val kiszednem
+}
+
+const inputEvent = (event) => {
+    console.log(event.target.value); //csak az input mezőknek van value-ja
+    document.getElementById("inputValueContent").innerHTML = event.target.value;
+}
+
 function loadEvent() {
     const root = document.getElementById("root");
-    root.insertAdjacentHTML("beforeend", form);
+    root.insertAdjacentHTML("beforeend", formElement);
+    root.insertAdjacentHTML("beforeend", `
+    <div id="inputValueContent"></div>
+    `);
+
+    const form = document.getElementById("form"); //ez csak azért működik, mert létrehoztam előtte a formot
+    form.addEventListener("submit", formSubmit);
+    
+    const inputList = form.querySelectorAll("input");
+    for (const input of inputList) {
+        input.addEventListener("input", inputEvent)
+    }
 }
 
 window.addEventListener("load", loadEvent);
