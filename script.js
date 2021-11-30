04f200f97e34412bf26b023f9a8d8354ea3c9309
Cserne <angeli.cserne@gmail.com>
2021-11-30 09:43:33 +0100

    Changes added.

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function loadEvent() {
+/*
+Függvények létrehozásának 3 módja (amit változóban hozok létre, az csak miután elkészült, érhető el, a "sima" előbb is elérhető):
+
+function functionName() {
 
 }
 
-window.addEventListener("load", loadEvent)
+const functionName = function () {
+
+}
+
+const functionName = () => {
+
+}
+*/
+
+const input = `
+    <input type="text">
+`;
+
+const form = `
+    <form id="form">
+        ${input("text")}
+    </form>
+`;
+
+function loadEvent() {
+    const root = document.getElementById("root");
+    root.insertAdjacentHTML("beforeend", form);
+}
+
+window.addEventListener("load", loadEvent);
